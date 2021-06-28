a9450e424b71a1ed701f50f96b85028ec160a358
A.Tuan <nguyentronganhtuan2102@gmail.com>
2021-06-29 01:57:30 +0700

    second change

--- Hello.cpp
+++ Hello.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    int a,b,c;
-    cin>>a>>b;
-    c=a+b;
-    cout<<c<<"ok";
+    int a;
+   
+   cin>>a;
+   int b=a*5;
+    cout<<b;
     return 0;
 }
